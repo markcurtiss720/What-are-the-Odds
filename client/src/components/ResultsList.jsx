45ffc52a0bf9344b76c3270c6df0e6f5a5847168
Mark Curtiss <markcurtiss720@gmail.com>
2024-02-08 19:48:31 -0700

    API is working and data is being displayed

--- client/src/components/ResultsList.jsx
+++ client/src/components/ResultsList.jsx
@@ -1,7 +1,10 @@
 function ResultList(props) {
+  console.log("testing props")
+  console.log(props)
     return (
       <ul className="list-group">
-        {props.odds?.map((odd) => (
+        hekhrjkeljrkelwjrkelwjrkewlj
+        {props.odds.map((odd) => (
           <li className="list-group-item" key={odd.id}>
             {odd.sport_title}
           </li>
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 
 
-const getBasketballOdds = (query) => {
-return axios.get(`https://api.the-odds-api.com/v4/sports/${query}/odds/?apiKey=657096347f9cea90f9f8a5b84dc4de0e&regions=us&markets=h2h`)
-.then(
-  response => {
-    console.log(response.data)
-    return response.data
+const getBasketballOdds = async (query) => {
+  try {
+const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${query}/odds/?apiKey=657096347f9cea90f9f8a5b84dc4de0e&regions=us&markets=h2h`)
+console.log("testing response");
+console.log(response);
+return response.data;
+  } catch (error) {
+    console.error('it looks like collin messed up this code, sorry', error);
+    throw error;
   }
+
+
+
+
+// .then(
+//   (response) => {
+//     console.log("testing response ")
+//     console.log(response.data)
+//     return response.data
+//   }
   
-);
+// );
 }
 
 
