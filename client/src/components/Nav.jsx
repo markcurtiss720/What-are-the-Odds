3f60c839a9aa10d0595285d21cd940147c76403a
Mark Curtiss <markcurtiss720@gmail.com>
2024-02-13 10:40:39 -0700

    Nav bar now shows different things in both the regular nav bar and the mobile navbar depending on if you are logged in or not

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Auth from "../utils/auth"
 import {
   Navbar,
-  MobileNav,
+  Collapse,
   Typography,
   Button,
   IconButton,
@@ -37,63 +37,89 @@ export default function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">
-              {account}
-            </div>
+      <Navbar className="fixed top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+          <div className="flex items-center justify-between text-blue-gray-900">
+            <Typography
+              as="a"
+              href="/"
+              className="mr-4 cursor-pointer py-1.5 font-medium">
+              🤑 What are the Odds?
+            </Typography>
+              <nav>
+                <div className="flex items-center gap-4">
+                <div className="mr-4 hidden lg:block">
+                  {account}
+                </div>
+                <div className="flex items-center gap-x-1">
+                  <SportsMenu/>
+                  <a href="/" onClick={() => Auth.logout()}>
+                    <Button
+                      variant="text"
+                      size="sm"
+                      className="hidden lg:inline-block">
+                      <span>Log out</span>
+                    </Button>
+                  </a>
+                </div>
+                  <IconButton
+                  variant="text"
+                  className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                  ripple={false}
+                  onClick={() => setOpenNav(!openNav)}>
+                  {openNav ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      className="h-6 w-6"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
+                    ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth={2}>
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M4 6h16M4 12h16M4 18h16"/>
+                    </svg>
+                      )}
+                  </IconButton>
+                </div>
+              </nav>
+            </div> 
+          <Collapse open={openNav}>
+            {account}
             <div className="flex items-center gap-x-1">
-            <SportsMenu/>
-            <a href="/" onClick={() => Auth.logout()}>
-              <Button
-                variant="text"
-                size="sm"
-                className="hidden lg:inline-block"
-              >
-                <span>Log out</span>
-              </Button>
-            </a>
+              <a href="/" onClick={() => Auth.logout()}>
+                <Button fullWidth variant="text" size="sm" className="">
+                  <span>Log out</span>
+                </Button>
+              </a>
             </div>
-            <IconButton
-              variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-              ripple={false}
-              onClick={() => setOpenNav(!openNav)}
-            >
-              {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </IconButton>
-        </div>
+          </Collapse>
+      </Navbar>
       )
     } else {
       return(
+        <Navbar className="fixed top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+        <div className="flex items-center justify-between text-blue-gray-900">
+          <Typography
+            as="a"
+            href="/"
+            className="mr-4 cursor-pointer py-1.5 font-medium"
+          >
+            🤑 What are the Odds?
+          </Typography>
+        <nav>
         <div className="flex items-center gap-4">
         <div className="flex items-center gap-x-1">
         <SportsMenu/>
@@ -154,30 +180,9 @@ export default function Nav() {
           )}
         </IconButton>
       </div>
-      )
-    }
-  }
- 
-  return (
-      <Navbar className="fixed top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="/"
-            className="mr-4 cursor-pointer py-1.5 font-medium"
-          >
-            🤑 What are the Odds?
-          </Typography>
-
-          <nav>
-            {showNavigation()}
-          </nav>
-        </div> 
-
-
-{/* this needs work, needs to different depending on if you */}
-        <MobileNav open={openNav}>
-          {account}
+      </nav>
+      </div>
+      <Collapse open={openNav}>
           <div className="flex items-center gap-x-1">
             <a href="/login">
             <Button fullWidth variant="text" size="sm" className="">
@@ -190,7 +195,15 @@ export default function Nav() {
             </Button>
             </a>
           </div>
-        </MobileNav>
+        </Collapse>
       </Navbar>
+      )
+    }
+  }
+
+  return (
+<div>
+{showNavigation()}
+</div>
   );
 }
