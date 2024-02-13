2077ccd5f07f64f395ee0a76137c798eabf5643a
Tomas Vargas <tavargas9@gmail.com>
2024-02-12 20:18:28 -0700

    individual sport page working

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 
 const getOdds = async (query) => {
   try {
-const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${query}/odds/?apiKey=657096347f9cea90f9f8a5b84dc4de0e&regions=us&markets=h2h`)
-console.log("testing response");
-console.log(response);
-return response.data;
+    const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${query}/odds/?apiKey=aa3becac25fcd2c2ab5fbeb0900b9847&regions=us&markets=h2h`)
+    console.log("testing response");
+    console.log(response);
+    return response.data;
   } catch (error) {
     console.error('it looks like collin messed up this code, sorry', error);
     throw error;
